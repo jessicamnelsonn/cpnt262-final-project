862f861e54e361193bfa4c3e38c5712096af368e
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-15 10:35:04 -0700

    added images

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,63 +1,63 @@
 export const gallery = [
   {
     title: '', 
-    image: '',
+    image: './images/food-1.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-2.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-3.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-4.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-5.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-6.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-7.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-8.jpg',
     description: '',
     url: '',
     alt: ''
   },
   {
     title: '', 
-    image: '',
+    image: './images/food-9.jpg',
     description: '',
     url: '',
     alt: ''
