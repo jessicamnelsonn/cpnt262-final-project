6e5d28f40f29ca5a0dfcff21ec81540db7254434
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-15 12:59:11 -0700

    fixing conflict

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
-<header>
-<h1 class="text-4xl flex justify-center">Cucina Bella</h1>
-<img class="bg-cover h-full w-full" src="images/hero-image.jpg" alt="">
-</header>
-
-<header class="max-w-full grid grid-cols-2 grid-rows-1 ">
+<header class="max-w-full grid grid-cols-2 grid-rows-1 bg-cover">
   <div class="typewriter text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center font-extrabold">
-    <h1>Code Coven.</h1>
-  </div>
-  <img class="col-span-full col-start-1 row-start-1 brightness-75" src="assets/images/federico-beccari-L8126OwlroY-unsplash.jpg" alt="starry sky at night">
+<h1>Cucina Bella</h1>
+</div>
+<img class="col-span-full col-start-1 row-start-1 brightness-75" src="images/hero-image.jpg" alt="">
 </header>
