111ba27ecf6e548a7ce7ec632c756cf5c267d76a
Dorothy Nguyen <dorothyjij@gmail.com>
2023-11-15 12:55:14 -0700

    ME dumb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,3 @@
 <h1 class="text-4xl flex justify-center">Cucina Bella</h1>
 <img class="bg-cover h-full w-full" src="images/hero-image.jpg" alt="">
 </header>
-
-<header class="max-w-full grid grid-cols-2 grid-rows-1 ">
-  <div class="typewriter text-slate-50 col-start-1 row-start-1 z-10 text-7xl justify-self-end self-center font-extrabold">
-    <h1>Code Coven.</h1>
-  </div>
-  <img class="col-span-full col-start-1 row-start-1 brightness-75" src="assets/images/federico-beccari-L8126OwlroY-unsplash.jpg" alt="starry sky at night">
-</header>
