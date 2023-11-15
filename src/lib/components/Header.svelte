1f6d107aa8715e6bb6d321200a9f553f1d6440c4
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-15 10:42:42 -0700

    added header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+  const navItems = [
+		{ name: '', href: '/' },
+		{ name: 'About', href: '/about' },
+		{ name: 'Menu', href: '/menu' },
+		{ name: 'Contact', href: '/contact' }
+	];
+</script>
+
+<header>
+	<nav>
+		<ul>
+			{#each navItems as { name, href }}
+				<li><a {href}>{name}</a></li>
+			{/each}
+		</ul>
+	</nav>
+</header>
