c853a7cc3ef3cc0f32e38f8e936aa4b2ea6e25c4
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-21 15:34:29 -0700

    styled header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,14 +3,15 @@
 		{ name: '', href: '/' },
 		{ name: 'Menu', href: '/menu' },
 		{ name: 'Contact', href: '/contact' },
+		{ name: 'About', href: '/about' },
     { name: 'Reservations', href: '/reservations' }
 	];
 </script>
 
-<header class="flex justify-between p-4">
+<header class="flex justify-between p-4 bg-black text-white">
   <div><a class="font-playfairDisplay text-4xl" href='/'>CB</a></div>
 	<nav>
-		<ul class="flex justify-around gap-4 gap-4">
+		<ul class="flex justify-around gap-12">
 			{#each navItems as { name, href }}
 				<li><a {href}>{name}</a></li>
 			{/each}
