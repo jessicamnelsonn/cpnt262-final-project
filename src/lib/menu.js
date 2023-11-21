604099eb1b93f9f6701978ccf1d651f0fc084387
Dorothy Nguyen <dorothyjij@gmail.com>
2023-11-20 18:45:30 -0700

    Added more text info

--- src/lib/menu.js
+++ src/lib/menu.js
@@ -32,41 +32,41 @@ export const menu = [
     alt: ''
   },
   {
-    title: 'Name', 
+    title: 'Tuscan Artichoke Symphony', 
     image: './images/food-5.jpg',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
+    description: 'Embark on a culinary journey with our Tuscan Artichoke Symphony, featuring tender artichoke hearts harmonizing with sun-dried tomatoes, garlic, and a medley of Italian herbs. This flavorful dish captures the essence of Tuscany, creating a symphony of taste that celebrates the art of Italian gastronomy.',
     price: '$',
     url: '',
     alt: ''
   },
   {
-    title: 'Name', 
+    title: 'Riviera Medley', 
     image: './images/food-6.jpg',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
+    description: 'Savor Italy with our Insalata Italiana—a vibrant mix of Romaine lettuce, cherry tomatoes, cucumber, mozzarella, Kalamata olives, and Parmesan, dressed in basil-infused olive oil. A burst of Mediterranean freshness awaits',
     price: '$',
     url: '',
     alt: ''
   },
   {
-    title: 'Name', 
+    title: 'Amaretto Infused Panna Cotta', 
     image: './images/food-7.jpg',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
+    description: 'Elevate your dessert experience with our Amaretto Infused Panna Cotta, a velvety masterpiece infused with the rich essence of amaretto. Silky-smooth and delicately set, this Italian classic is topped with a luscious berry compote, creating a symphony of indulgence that captures the essence of sweet sophistication.',
     price: '$',
     url: '',
     alt: ''
   },
   {
-    title: 'Name', 
+    title: 'Italian Meringue Perfection', 
     image: './images/food-8.jpg',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
+    description: 'Indulge in the allure of our Italian Meringue Perfection—a delicate fusion of whipped egg whites and sugar, torched to golden perfection. This sweet masterpiece, reminiscent of the Italian culinary finesse, offers a cloud-like texture with a hint of vanilla, ensuring a sublime finale to your dining experience.',
     price: '$',
     url: '',
     alt: ''
   },
   {
-    title: 'Name', 
+    title: 'Tricolore Rustica Pizza', 
     image: './images/food-9.jpg',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
+    description: 'Immerse yourself in a culinary masterpiece with our Tricolore Rustica Pizza, where the savory trio of tomato, fresh basil, and mozzarella harmonize atop a rustic crust. Enhanced by Kalamata olives and artichoke hearts, its a symphony of Mediterranean flavors that transports you to the heart of Italy.',
     price: '$',
     url: '',
     alt: ''
