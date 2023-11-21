e7d90cee00d6e7c6d86451f0681d3abddb08b679
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-21 15:51:45 -0700

    styled with custom fonts

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,7 +11,7 @@
 <header class="flex justify-between p-4 bg-black text-white">
   <div><a class="font-playfairDisplay text-4xl" href='/'>CB</a></div>
 	<nav>
-		<ul class="flex justify-around gap-12">
+		<ul class="flex justify-around gap-12 font-lato">
 			{#each navItems as { name, href }}
 				<li><a {href}>{name}</a></li>
 			{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <div class="relative">
   <img class="brightness-75" src="images/hero-image.jpg" alt="">
   <section class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white">
-    <h1 class="text-8xl font-playfairDisplay text-white">Cucina Bella</h1>
+    <h1 class="font-playfairDisplay text-8xl text-white">Cucina Bella</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptates fugit doloribus tenetur ex quod. Velit aliquam maiores alias nihil.</p>
-    <button class="m-4 px-4 py-2 bg-red-700 text-white hover:bg-red-800 active:bg-red-900">
+    <button class="font-lato m-4 px-4 py-2 bg-red-700 text-white hover:bg-red-800 active:bg-red-900">
       Reservations
     </button>
     
