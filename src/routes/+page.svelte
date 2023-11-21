045107ab73ee594ec289f72aadc9d3b7d930218f
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-20 23:31:47 -0700

    added cta button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <div class="relative">
   <img class="brightness-75" src="images/hero-image.jpg" alt="">
-  <div class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white">
-    <h1 class="text-7xl font-playfairDisplay text-white">Cucina Bella</h1>
-  </div>
+  <section class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center text-white">
+    <h1 class="text-8xl font-playfairDisplay text-white">Cucina Bella</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptates fugit doloribus tenetur ex quod. Velit aliquam maiores alias nihil.</p>
+    <button class="m-4 px-4 py-2 bg-red-700 text-white hover:bg-red-800 active:bg-red-900">
+      Reservations
+    </button>
+    
+  </section>
 </div>
 
