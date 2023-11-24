675ea38533318256a778d00d092dce6d8307fad7
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-24 15:40:42 -0700

    added extended info and styling

--- src/routes/menu/[slug]/+page.svelte
+++ src/routes/menu/[slug]/+page.svelte
@@ -11,9 +11,9 @@
   <section>
     <article class="p-4 rounded-lg shadow-md" style="width: 300px;">
       <img src={data.menuItem.image} alt={data.menuItem.alt} class="mb-2 rounded-md" style="width: 100%; height: auto;">
-      <h2 class="text-4xl flex justify-between font-playfairDisplay"><span>{data.menuItem.title}</span><span>{data.menuItem.price}</span></h2>
+      <h2 class="text-4xl flex justify-between font-playfairDisplay mb-2"><span class="font-bold">{data.menuItem.title}</span><span>{data.menuItem.price}</span></h2>
       <p class="font-lato mt-4">{data.menuItem.description}</p>
-      <p>Allergens: {data.menuItem.allergen}</p>
+      <p class="text-gray-400">Allergens: {data.menuItem.allergen}</p>
     </article>
   </section>
 </div>
