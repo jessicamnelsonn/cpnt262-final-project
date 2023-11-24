3f6f2faf0b878d091674b8b6ae1a49bc2a6f5427
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-24 16:22:58 -0700

    finished items page

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,7 +2,7 @@ export let italianMenu = [
   {
     title: 'Bella Bolognese Reverie', 
     slug: 'bolognese',
-    image: './images/food-1.jpg',
+    image: '/images/food-1.jpg',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
     price: '$',
     allergen: 'wheat, gluten, dairy, soy',
@@ -11,7 +11,7 @@ export let italianMenu = [
   {
     title: 'Bianco Velvet Ravioli', 
     slug:'ravioli',
-    image: './images/food-2.jpg',
+    image: '/images/food-2.jpg',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
     price: '$',
     allergen: 'wheat, gluten, dairy, soy',
@@ -20,7 +20,7 @@ export let italianMenu = [
   {
     title: 'Penne Primavera Perfection', 
     slug: 'penne',
-    image: './images/food-3.jpg',
+    image: '/images/food-3.jpg',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, earum.',
     price: '$',
     allergen: 'wheat, gluten, dairy, soy',
@@ -29,7 +29,7 @@ export let italianMenu = [
   {
     title: 'Sicilian Citrus Infused Oysters', 
     slug: 'oysters',
-    image: './images/food-4.jpg',
+    image: '/images/food-4.jpg',
     description: 'Savor the sea with our Sicilian Citrus Infused Oysters—a tantalizing blend of fresh oysters kissed by Sicilys sun-soaked citrus notes. Enhanced with a touch of garlic and herbs, each bite is a journey to the Sicilian coastline, offering a zesty and briny symphony.',
     price: '$',
     allergen: 'shellfish',
@@ -38,7 +38,7 @@ export let italianMenu = [
   {
     title: 'Tuscan Artichoke Symphony', 
     slug: 'artichoke',
-    image: './images/food-5.jpg',
+    image: '/images/food-5.jpg',
     description: 'Embark on a culinary journey with our Tuscan Artichoke Symphony, featuring tender artichoke hearts harmonizing with sun-dried tomatoes, garlic, and a medley of Italian herbs. This flavorful dish captures the essence of Tuscany, creating a symphony of taste that celebrates the art of Italian gastronomy.',
     price: '$',
     allergen: 'may contain: soy, dairy',
@@ -47,7 +47,7 @@ export let italianMenu = [
   {
     title: 'Riviera Medley', 
     slug: 'spring-salad',
-    image: './images/food-6.jpg',
+    image: '/images/food-6.jpg',
     description: 'Savor Italy with our Insalata Italiana—a vibrant mix of Romaine lettuce, cherry tomatoes, cucumber, mozzarella, Kalamata olives, and Parmesan, dressed in basil-infused olive oil. A burst of Mediterranean freshness awaits',
     price: '$',
     allergen: 'dairy',
@@ -56,7 +56,7 @@ export let italianMenu = [
   {
     title: 'Amaretto Infused Panna Cotta', 
     slug: 'panna-cotta',
-    image: './images/food-7.jpg',
+    image: '/images/food-7.jpg',
     description: 'Elevate your dessert experience with our Amaretto Infused Panna Cotta, a velvety masterpiece infused with the rich essence of amaretto. Silky-smooth and delicately set, this Italian classic is topped with a luscious berry compote, creating a symphony of indulgence that captures the essence of sweet sophistication.',
     price: '$',
     allergen: 'dairy',
@@ -65,7 +65,7 @@ export let italianMenu = [
   {
     title: 'Italian Meringue Perfection', 
     slug: 'meringue',
-    image: './images/food-8.jpg',
+    image: '/images/food-8.jpg',
     description: 'Indulge in the allure of our Italian Meringue Perfection—a delicate fusion of whipped egg whites and sugar, torched to golden perfection. This sweet masterpiece, reminiscent of the Italian culinary finesse, offers a cloud-like texture with a hint of vanilla, ensuring a sublime finale to your dining experience.',
     price: '$',
     allergen: 'eggs',
@@ -74,7 +74,7 @@ export let italianMenu = [
   {
     title: 'Tricolore Rustica Pizza', 
     slug: 'pizza',
-    image: './images/food-9.jpg',
+    image: '/images/food-9.jpg',
     description: 'Immerse yourself in a culinary masterpiece with our Tricolore Rustica Pizza, where the savory trio of tomato, fresh basil, and mozzarella harmonize atop a rustic crust. Enhanced by Kalamata olives and artichoke hearts, its a symphony of Mediterranean flavors that transports you to the heart of Italy.',
     price: '$',
     allergen: 'wheat, dairy',
--- src/routes/menu/[slug]/+page.svelte
+++ src/routes/menu/[slug]/+page.svelte
@@ -7,12 +7,17 @@
   <title>{data.menuItem.title}</title>
 </svelte:head>
 
-<div class="text-white bg-black">
+<div class="text-white bg-black min-h-screen flex items-center justify-center">
   <section>
-    <article class="p-4 rounded-lg shadow-md" style="width: 300px;">
-      <img src={data.menuItem.image} alt={data.menuItem.alt} class="mb-2 rounded-md" style="width: 100%; height: auto;">
-      <h2 class="text-4xl flex justify-between font-playfairDisplay mb-2"><span class="font-bold">{data.menuItem.title}</span><span>{data.menuItem.price}</span></h2>
-      <p class="font-lato mt-4">{data.menuItem.description}</p>
+    <article class="p-4 rounded-lg shadow-md bg-white text-black" style="width: 500px;">
+      <img src={data.menuItem.image} alt={data.menuItem.alt} class="mb-4 rounded-lg" style="width: 100%; height: auto;">
+
+      <h2 class="text-2xl font-bold mb-2 flex justify-between">
+        <span class=" font-playfairDisplay">{data.menuItem.title}</span>
+        <span>{data.menuItem.price}</span>
+      </h2>
+
+      <p class="font-lato text-gray-900 mb-4">{data.menuItem.description}</p>
       <p class="text-gray-400">Allergens: {data.menuItem.allergen}</p>
     </article>
   </section>
