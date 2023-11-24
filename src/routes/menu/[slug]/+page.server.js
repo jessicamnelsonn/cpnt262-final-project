0c3cc49f7d63af8862d0e5a3bdda77f7d8bdb7f1
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-24 15:02:09 -0700

    added error message

--- src/routes/menu/[slug]/+page.server.js
+++ src/routes/menu/[slug]/+page.server.js
@@ -1,8 +1,11 @@
 import { italianMenu } from '../../../lib/data.js';
+import {error} from "@sveltejs/kit"
 
 export function load({ params }) {
   const menuItem = italianMenu.find((menuItem) => menuItem.slug === params.slug);
   
+  if (!menuItem) throw error(404);
+
 	return {
       menuItem
   };
