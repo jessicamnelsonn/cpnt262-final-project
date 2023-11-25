11c6505a6ffa669421938c277922bbe2d70a0a84
Jessica Nelson <jessica.nelson60@gmail.com>
2023-11-24 18:15:43 -0700

    final finalization

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,51 +2,27 @@ export let italianMenu = [
   {
     title: 'Bella Bolognese Reverie', 
     slug: 'bolognese',
-<<<<<<< HEAD
-    image: './images/food-1.jpg',
-    description: 'Embark on a culinary journey with "Bella Bolognese Reverie," a symphony of slow-cooked beef and pork ragù, harmonizing with aromatic herbs and tomatoes. Immerse yourself in the allure of Italian flavors',
-    alt: 'top view of a bolognese pasta on a plate ',
-    image: './images/food-1.jpg',
-    price: '$19',
-=======
     image: '/images/food-1.jpg',
     description: 'Savor the blissful notes of our Bella Bolognese Reverie. Dive into perfectly al dente pasta cradled in a luscious, slow-simmered Bolognese sauce. Each bite is a symphony of robust meats, aromatic herbs, and pure Italian delight.',
     price: '$24',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'wheat, gluten, dairy, soy',
     alt: 'top view of a bolognese pasta on a plate'
   },
   {
     title: 'Bianco Velvet Ravioli', 
     slug:'ravioli',
-<<<<<<< HEAD
-    image: './images/food-2.jpg',
-    description: 'Experience the sublime with our "Bianco Velvet Bliss Ravioli." Each pasta pocket cradles a luxurious ricotta and spinach filling, embraced by a velvety white sauce for a taste of pure culinary indulgence.',
-    image: './images/food-2.jpg',
-    price: '$17',
-=======
     image: '/images/food-2.jpg',
     description: 'Experience the sublime with our "Bianco Velvet Bliss Ravioli." Each pasta pocket cradles a luxurious ricotta and spinach filling, embraced by a velvety white sauce for a taste of pure culinary indulgence.',
     price: '$24',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'wheat, gluten, dairy, soy',
     alt: 'top view of ravioli in a creamy parmesan sauce on a plate'
   },
   {
     title: 'Penne Primavera Perfection', 
     slug: 'penne',
-<<<<<<< HEAD
-    image: './images/food-3.jpg',
-    description: 'Delight in our "Penne Primavera Perfection," a symphony of al dente penne pasta tossed with vibrant seasonal vegetables, sun-drenched tomatoes, and a medley of aromatic herbs. A celebration of Italian simplicity and freshness',
-    price: '$24',
-    image: '/images/food-3.jpg',
-    description: 'Satisfy your cravings with our Penne Pasta in Creamy Garlic Sauce. Delight in perfectly cooked penne enveloped in a velvety blend of rich cream and aromatic garlic.',
-    price: '$17',
-=======
     image: '/images/food-3.jpg',
     description: 'Delight in our "Penne Primavera Perfection," a symphony of al dente penne pasta tossed with vibrant seasonal vegetables, sun-drenched tomatoes, and a medley of aromatic herbs. A celebration of Italian simplicity and freshness',
     price: '$24',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'wheat, gluten, dairy, soy',
     alt: 'top view of a penne pasta in a creamy garlic sauce on a plate'
   },
@@ -55,11 +31,7 @@ export let italianMenu = [
     slug: 'oysters',
     image: '/images/food-4.jpg',
     description: 'Savor the sea with our Sicilian Citrus Infused Oysters—a tantalizing blend of fresh oysters kissed by Sicilys sun-soaked citrus notes. Enhanced with a touch of garlic and herbs, each bite is a journey to the Sicilian coastline, offering a zesty and briny symphony.',
-<<<<<<< HEAD
-    price: '$22',
-=======
     price: '$28',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'shellfish',
     alt: 'top view of an oyster dish with pieces of spinach on a plate'
   },
@@ -68,13 +40,8 @@ export let italianMenu = [
     slug: 'artichoke',
     image: '/images/food-5.jpg',
     description: 'Embark on a culinary journey with our Tuscan Artichoke Symphony, featuring tender artichoke hearts harmonizing with sun-dried tomatoes, garlic, and a medley of Italian herbs. This flavorful dish captures the essence of Tuscany, creating a symphony of taste that celebrates the art of Italian gastronomy.',
-<<<<<<< HEAD
-    image: '/images/food-5.jpg',
-    price: '$16',
-=======
     price: '$22',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
-    allergen: 'may contain: soy, dairy',
+    allergen: 'dairy, soy',
     alt: 'top view of an artichoke dish on a plate'
   },
   {
@@ -82,12 +49,7 @@ export let italianMenu = [
     slug: 'spring-salad',
     image: '/images/food-6.jpg',
     description: 'Savor Italy with our Insalata Italiana—a vibrant mix of Romaine lettuce, cherry tomatoes, cucumber, mozzarella, Kalamata olives, and Parmesan, dressed in basil-infused olive oil. A burst of Mediterranean freshness awaits',
-<<<<<<< HEAD
-    image: '/images/food-6.jpg',
-    price: '$15',
-=======
     price: '$20',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'dairy',
     alt: 'top view of a spring salad on a plate'
   },
@@ -96,12 +58,7 @@ export let italianMenu = [
     slug: 'panna-cotta',
     image: '/images/food-7.jpg',
     description: 'Elevate your dessert experience with our Amaretto Infused Panna Cotta, a velvety masterpiece infused with the rich essence of amaretto. Silky-smooth and delicately set, this Italian classic is topped with a luscious berry compote, creating a symphony of indulgence that captures the essence of sweet sophistication.',
-<<<<<<< HEAD
-    image: '/images/food-7.jpg',
-    price: '$12',
-=======
     price: '$22',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'dairy',
     alt: 'top view of a panna cotta on a plate with berries drizzled on top'
   },
@@ -110,12 +67,7 @@ export let italianMenu = [
     slug: 'meringue',
     image: '/images/food-8.jpg',
     description: 'Indulge in the allure of our Italian Meringue Perfection—a delicate fusion of whipped egg whites and sugar, torched to golden perfection. This sweet masterpiece, reminiscent of the Italian culinary finesse, offers a cloud-like texture with a hint of vanilla, ensuring a sublime finale to your dining experience.',
-<<<<<<< HEAD
-    image: '/images/food-8.jpg',
-    price: '$12',
-=======
     price: '$22',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'eggs',
     alt: 'top view of a meringue on a plate with a strawberry'
   },
@@ -124,12 +76,7 @@ export let italianMenu = [
     slug: 'pizza',
     image: '/images/food-9.jpg',
     description: 'Immerse yourself in a culinary masterpiece with our Tricolore Rustica Pizza, where the savory trio of tomato, fresh basil, and mozzarella harmonize atop a rustic crust. Enhanced by Kalamata olives and artichoke hearts, its a symphony of Mediterranean flavors that transports you to the heart of Italy.',
-<<<<<<< HEAD
-    image: '/images/food-9.jpg',
-    price: '$18',
-=======
     price: '$25',
->>>>>>> 48cf5cd4f44719c9bad4a3eaa22ff4d037860806
     allergen: 'wheat, dairy',
     alt: 'top view of pizza on a plate'
   },
